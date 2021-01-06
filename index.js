cd7a3cfdb6337f9b20162f68aa102d81be985552
jinwood <jinw@protonmail.com>
2021-01-06 22:10:53 +0000

    Text

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import {factory} from './modules/factory';
+// import {factory} from './modules/factory';
 
 const SELECTORS = {
-  SELECTION_INPUT = "#selection-input",
-  RUN_BTN = "#selection-run",
-  RESULT = "#result",
+  SELECTION_INPUT: "selection-input",
+  RUN_BTN: "selection-run",
+  RESULT: "result",
 };
 
 const selection = document.getElementById(SELECTORS.SELECTION_INPUT);
-const resultSp = document.getElementById(SELECTORS.RESULT).textContent("");
+const resultElement = document.getElementById(SELECTORS.RESULT);
+console.log(selection);
 
-const displayResult = (resultText) => resultSp.textContent(resultText);
+const displayResult = (resultText) => (resultElement.textContent = resultText);
 
-switch(selection) {
-  case 0:
-  default:
-    displayResult("Something bad happened")
-    break;
+function buttonClick() {
+  console.log("button click");
+  switch (selection) {
+    case 1:
+      displayResult("1");
+      break;
+    case 0:
+    default:
+      displayResult("Something bad happened");
+      break;
+  }
 }
